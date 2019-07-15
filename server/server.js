3bd07cc8b9ef1e8575ae43bbc7402dd2c75c9cbd
amiltonbrune <amilton.junior@digisystem.com.br>
2019-07-15 16:45:15 -0300

    Subindo aplicação finalizada

---
+++ server/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const config = require('./config/keys.config.js');
+const TaskRoutes = require('./routes/Task');
+const cors = require('cors');
+
+const app = express();
+
+mongoose.connect(config.mongodbUri, { useNewUrlParser: true })
+    .then(() => console.log('Database connected'))
+    .catch((err) => console.log('Error on connection', err));
+
+mongoose.set('useFindAndModify', false);
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.use('/api/tasks', TaskRoutes);
+
+const port = process.env.PORT || 3001;
+
+app.listen(port, function () {
+    console.log('Server listening on port ' + port);
+});
